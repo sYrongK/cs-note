57cda1f4b68d3c7f070ad1bf2f98160823cad83a
SYrongK <sYrongK@gmail.com>
2021-07-17 11:08:29 +0900

    Initial commit

---
+++ src/com/javanote/cs/Main.java
@@ -0,0 +1,13 @@
+package com.javanote.cs;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        /*
+         * stack의 사이즈
+         * stack의 최상단 top
+         * */
+    }
+}
